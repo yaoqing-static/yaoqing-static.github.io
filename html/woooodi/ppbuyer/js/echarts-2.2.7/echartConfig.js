59b73be4dc52d2e6d3edc9cf174ab43b7ae3a1b9
yaoqing <yaoqing@haizitong.com>
2019-03-12 10:15:05 +0800

    静态文件

---
+++ html/woooodi/ppbuyer/js/echarts-2.2.7/echartConfig.js
@@ -0,0 +1,24 @@
+require.config({
+    paths:{
+        echarts: 'js/echarts-2.2.7/build/dist'
+    }
+});
+var requireDemo=[
+    'echarts',
+    'echarts/chart/line'/*,
+    'echarts/chart/bar',
+    'echarts/chart/scatter',
+    'echarts/chart/k',
+    'echarts/chart/pie',
+    'echarts/chart/radar',
+    'echarts/chart/force',
+    'echarts/chart/chord',
+    'echarts/chart/map',
+    'echarts/chart/gauge',
+    'echarts/chart/funnel',
+    'echarts/chart/venn',
+    'echarts/chart/treemap',
+    'echarts/chart/tree',
+    'echarts/chart/eventRiver'*/
+];
+
